fcf4f5ffe8b64372869b76ed86c60d2848f1277c
Jendrula95 <93941807+Jendrula95@users.noreply.github.com>
2022-03-23 12:40:16 +0100

    Add files via upload

---
+++ lab2/lab2/Classroom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    internal class Classroom
+    {
+        private string name;
+        private Person[] person;
+        public string  Name { get { return name; } }
+        public Classroom(string name, Person[] person)
+        {
+            this.name = name;
+            this.person = person;
+        }
+        public override string ToString()
+        {
+            string results = "Classroom : " + this.name + "\n\n";
+            foreach (Person p in person)
+            {
+                results += results + p.ToString();
+            }
+            return results;
+        }
+    }
+    
+}
